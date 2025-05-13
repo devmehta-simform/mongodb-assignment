e0dba660b4af69173613d5de4b4c653e8a890f3a
devmehta-simform <dev.mehta@simformsolutions.com>
2025-05-13 16:49:02 +0530

    fix: loginUser returns user

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -4,9 +4,9 @@ import { middlewareWrapper } from '../utils/asyncMiddlewareWrapper';
 export const loginUser = middlewareWrapper(async (req, res, next) => {
   const password = 'Simform123';
   const { email: userEmail, password: userPassword } = req.body;
-  const userExists = await UserModel.exists({ email: userEmail });
-  if (userExists !== null && userPassword === password) {
-    return res.status(200).json();
+  const user = await UserModel.findOne({ email: userEmail });
+  if (user && userPassword === password) {
+    return res.status(200).json(user);
   }
   return res.status(401).json('wrong credentials');
 });
